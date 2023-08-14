bb4ab38e7dc2c42e40e1d787d99c13485668a73d
Nicolas 'Grobim' Chalet <nicolas.chalet@gmail.com>
2023-08-14 18:28:51 +0200

    test new branch

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Flemme de m'introduire</p>
+        <p>Franchement flemme de m'introduire</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
